9949fb9bd4e48f272ba22bf8a2640c8804e19647
Aarghyadeep Das <aarghyario98@gmail.com>
2024-02-07 23:15:40 +0530

    headers added

--- Server/index.js
+++ Server/index.js
@@ -18,7 +18,7 @@ app.use(cors({origin:"https://alpha55blogs.netlify.app",
               headers: {
                 "Access-Control-Allow-Origin" : "https://alpha55blogs.netlify.app",
                 "Access-Control-Allow-Credentials" : true,
-                "Access-Control-Allow-Headers": content-type
+                "Access-Control-Allow-Headers" : "content-type",
               },
               credentials: true}));
 app.use("/images",express.static(path.join(__dirname,"/images")));
